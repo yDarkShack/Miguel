f8a6386395f92955ba4cfb621a64c6f0e452c290
yDarkShack <99296971+yDarkShack@users.noreply.github.com>
2022-02-08 17:58:33 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var box 
+
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,20,50)
+}
+
+function draw() 
+{
+  background("purple");
+  if (keyDown("up")){
+    box.y = box.y -5
+  }
+  if (keyDown("left")){
+    box.x = box.x -5
+  }
+  if (keyDown("down")){
+    box.y = box.y +5
+  }
+  if (keyDown("right")){
+    box.x = box.x +5
+  }
+drawSprites()
+}
+
+
+
